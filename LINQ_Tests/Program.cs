155333a5e2e5013fa52a05e034a3b9310d7a829d
Stas9700 <42588887+Stas9700@users.noreply.github.com>
2023-06-25 22:07:47 +0300

    init

---
+++ LINQ_Tests/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var list = new List<int>(new [] { 1,2,3,4,}); 
+        var t = list.Select(s =>
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(++s);
+                return s;
+            })
+            .Where(w => w%2 == 0);
+        t.Select(s =>
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(s);
+            return s;
+        }).ToArray();
+    }
+}
